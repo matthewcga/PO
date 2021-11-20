310d11a48ea6c7aaee27790f88f955ea1517efe4
matthewcga <mateusz.cga@gmail.com>
2021-11-20 14:36:46 +0100

    lab 5 fix

--- po 05/src/main/java/agh/ics/oop/GrassFiled.java
+++ po 05/src/main/java/agh/ics/oop/GrassFiled.java
@@ -10,7 +10,8 @@ import java.util.Random;
 
 public class GrassFiled extends AbstractWorldMap {
 
-    private int grassRange;
+    private final int grassRange;
+    private final Random rand;
     private List<Grass> grass;
 
     public GrassFiled(int quantity) {
@@ -18,22 +19,19 @@ public class GrassFiled extends AbstractWorldMap {
         this.lowerLeft = new Vector2d(0, 0);
         this.upperRight = new Vector2d (0, 0);
         this.animals = new ArrayList<>();
-
-        var grassMap = new HashMap<Vector2d, Grass>();
-        var rand = new Random();
+        this.rand = new Random();
+        this.grass = new ArrayList<>();
 
         for (int i = 0; i < quantity; i++) {
             while (true) {
-                var position = new Vector2d(rand.nextInt(grassRange), rand.nextInt(grassRange));
-                if (!grassMap.containsKey(position)) {
-                    grassMap.put(position, new Grass(position));
-                    updateUpperRight(position);
+                var newPosition = new Vector2d(rand.nextInt(grassRange), rand.nextInt(grassRange));
+                if (!isOccupiedByGrass(newPosition)) {
+                    this.grass.add(new Grass(newPosition));
+                    updateUpperRight(newPosition);
                     break;
                 }
             }
         }
-
-        this.grass = new ArrayList<>(grassMap.values());
     }
 
     public void updateUpperRight(Vector2d position)
@@ -61,29 +59,26 @@ public class GrassFiled extends AbstractWorldMap {
         return false;
     }
 
-    public void plantGrass(Vector2d position) {
-        var grassMap = new HashMap<Vector2d, Grass>();
-        var rand = new Random();
-
-        for (Grass singleGrass : grass) grassMap.put(singleGrass.getPosition(), singleGrass);
-
-        while (true) {
-            var newPosition = new Vector2d(rand.nextInt(grassRange), rand.nextInt(grassRange));
-            if (!grassMap.containsKey(newPosition)) {
-                grassMap.put(newPosition, new Grass(position));
-                updateUpperRight(newPosition);
-                break;
-            }
-        }
+    public boolean isOccupiedByGrass(Vector2d position) {
+        for (Grass singleGrass : grass) if (singleGrass.getPosition().equals(position)) return true;
+        return false;
     }
 
     public boolean tryEat(Vector2d position) {
-        for (int i = 0; i < grass.size(); i++)
-            if (grass.get(i).getPosition().equals(position)) {
-                grass.remove(i);
-                plantGrass(position);
-                return true;
+        for (Grass singleGrass : grass) {
+            if (singleGrass.getPosition().equals(position)) {
+                grass.remove(singleGrass);
+
+                while (true) {
+                    var newPosition = new Vector2d(rand.nextInt(grassRange), rand.nextInt(grassRange));
+                    if (!isOccupied(newPosition)) {
+                        grass.add(new Grass(newPosition));
+                        updateUpperRight(newPosition);
+                        return true;
+                    }
+                }
             }
+        }
         return false;
     }
 
